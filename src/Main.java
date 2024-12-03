8d5bbbe60b0a2157ba2a356205ee450aab7bcf42
MariaSizz <buchacik@mail.ru>
2024-12-04 02:17:40 +0300

    Check HW.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author Tolle = new Author("Eckhart", "Tolle");
+        Author Carroll = new Author("Lee", "Carroll");
+
+        Book ANewEarth = new Book("A New Earth", Tolle, 2005);
+        Book TheJourneyHome = new Book("The Journey Home", Carroll, 1996);
+
+        TheJourneyHome.setYearOfPublication(1997);
+
+        System.out.println("Название " + ANewEarth.getBookName() + ", автор " + Tolle.getAuthorName() + ", год выпуска " + ANewEarth.getYearOfPublication());
+        System.out.println("Название " + TheJourneyHome.getBookName() + ", автор " + Carroll.getAuthorName() + ", год выпуска " + TheJourneyHome.getYearOfPublication());
     }
 }
